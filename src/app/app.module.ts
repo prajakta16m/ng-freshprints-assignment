66e1d54413e14903e3b883a486d9282b023ec72a
prajakta16m <prajakta.mahamuni91@gmail.com>
2023-02-10 15:44:54 +0530

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { UserService } from './user.service';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    CommonModule,
-    RouterModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, CommonModule, RouterModule, AppRoutingModule],
   bootstrap: [AppComponent],
-  declarations: [
-    AppComponent
-  ],
-  exports:[AppComponent]
+  declarations: [AppComponent],
+  exports: [AppComponent],
+  providers: [UserService],
 })
-export class AppModule { }
+export class AppModule {}
