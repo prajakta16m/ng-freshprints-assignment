a154b55ff47c7a6c23c98275588bf86cf050c6b6
prajakta16m <prajakta.mahamuni91@gmail.com>
2023-02-10 15:46:45 +0530

    userservice

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,10 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  URL = 'https://api.github.com/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
+  getUser(username: String) {
+    return this.http.get(`${this.URL}/${username}`);
+  }
 }
