6a611f4ac9f8b6ac9eeca49c7a2e37749688e375
prajakta16m <prajakta.mahamuni91@gmail.com>
2023-02-10 18:06:32 +0530

    local storage implemented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,29 +10,58 @@ import { UserService } from '../user.service';
 export class HomeComponent implements OnInit {
   public username: any = 'prajakta16m';
   public displayUser: User = null;
+  public storedUsers = [];
 
   constructor(private userService: UserService) {}
 
   search() {
-    if (this.username.length > 0) {
-      this.userService.getUser(this.username).subscribe((user) => {
-        console.log('success');
-        console.log(user);
+    if (this.username.length == 0) return;
 
+    this.displayUser = this.getUser();
+    if (this.displayUser == null) {
+      // Make API call
+      this.userService.getUser(this.username).subscribe(
+        (user) => {
         this.displayUser = {
           login: user['login'],
           name: user['name'],
           avatar: user['avatar_url'],
         };
 
-        console.log(this.displayUser);
-        /* const newUser = new User();
-        newUser.login = user['login'];
-        newUser.name = user['name'];
-        newUser.avatar = user['']; */
-      });
+        this.storedUsers.push(this.displayUser);
+        this.updateLocalStorage();
+      } , 
+      (error) => {
+        // API Error
+      }
+      );
     }
   }
 
-  ngOnInit() {}
+  updateLocalStorage() {
+    localStorage.setItem('users', JSON.stringify(this.storedUsers));
+  }
+
+  getUser() {
+    let temp = null;
+    console.log('su', this.storedUsers);
+    for (let item of this.storedUsers) {
+      if (item.login == this.username) {
+        temp = item;
+        break;
+      }
+    }
+    return temp;
+  }
+
+  ngOnInit() {
+    // If User exists in local storage, display ( save API call )
+    let usersList: any = localStorage.getItem('users');
+    if (usersList != null) {
+      this.storedUsers = JSON.parse(usersList);
+    } else {
+      usersList = [];
+      localStorage.setItem('users', usersList);
+    }
+  }
 }
