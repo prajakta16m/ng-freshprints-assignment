8cd24bd0f78e311c3a4bd40311c2118fbefdd49c
prajakta16m <prajakta.mahamuni91@gmail.com>
2023-02-10 17:26:41 +0530

    Home component ready

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -10,7 +10,7 @@ import { User } from '../user.model';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css'],
+  styleUrls: ['./user-details.component.scss'],
 })
 export class UserDetailsComponent implements OnInit, OnChanges {
   @Input() data: User;
